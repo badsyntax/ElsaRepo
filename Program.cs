9c6f4697ebd2d6578daeb92ca0045dc0fe54774e
Richard Willis <willis.rh@gmail.com>
2024-11-30 10:22:17 +0000

    Improve perf

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Elsa.Extensions;
 using Elsa.Workflows;
 using Elsa.Workflows.Management;
+using Elsa.Workflows.Middleware.Workflows;
 using Elsa.Workflows.Runtime;
 using ElsaConsole;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,7 +10,14 @@ var services = new ServiceCollection();
 // Add Elsa services to the container.
 services.AddElsa(elsa =>
 {
-   
+   elsa.UseWorkflows(workflows =>
+    {
+        workflows.WithWorkflowExecutionPipeline(pipeline => pipeline
+            .Reset()
+            .UseEngineExceptionHandling()
+            .UseExceptionHandling()
+            .UseDefaultActivityScheduler());
+    });
 });
 
 // Build the service container.
@@ -21,5 +29,5 @@ await registriesPopulator.PopulateAsync();
 var workflowRunner = serviceProvider.GetRequiredService<IWorkflowRunner>(); // Or inject it in your constructor.
 var workflowBuilderFactory = serviceProvider.GetRequiredService<IWorkflowBuilderFactory>();
 
+// await Example_MassiveDataSet.Run_Example1(workflowBuilderFactory, workflowRunner);
 await Example_MassiveDataSet.Run_Example1(workflowBuilderFactory, workflowRunner);
-// await Example_MassiveDataSet.Run_Example2(workflowBuilderFactory, workflowRunner);
